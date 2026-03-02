b001febe24e97235b95b8474834d7e9fbfee6194
Melcore <lapostolle.jean@gmail.com>
2026-03-02 07:03:46 +0100

    feat: prek config

--- blogpy/build.py
+++ blogpy/build.py
@@ -1,38 +1,35 @@
-import os
-import shutil
-from dataclasses import dataclass
 from pathlib import Path
 
 
 from datetime import datetime
 from jinja2 import Environment, PackageLoader, select_autoescape
 
-from blogpy.blogpy.article import Article
 from blogpy.blogpy.site import Site
 from blogpy.blogpy.utils import copy_static, get_article, write_html
 
-env = Environment(
-    loader=PackageLoader("blogpy"),
-    autoescape=select_autoescape()
-)
+env = Environment(loader=PackageLoader("blogpy"), autoescape=select_autoescape())
 
 
 def build(content_path, build_path):
-	content_path = Path(content_path)
-	build_path = Path(build_path)
-	site = Site(title="Blog de Melcore", description="Le blog de la connaissance et du avoir peu commun", year=datetime.now().year)
+    content_path = Path(content_path)
+    build_path = Path(build_path)
+    site = Site(
+        title="Blog de Melcore",
+        description="Le blog de la connaissance et du avoir peu commun",
+        year=datetime.now().year,
+    )
 
-	articles=[]
+    articles = []
 
-	copy_static(Path("blogpy/templates/static"), build_path / "static")
+    copy_static(Path("blogpy/templates/static"), build_path / "static")
 
-	for child in content_path.iterdir():
-		filename = child.stem
-		article = get_article(child)
-		article_template = env.get_template("article.html")
-		article.content = article_template.render(article=article.infos(), site=site)
-		write_html(build_path / "article" / (filename + ".html"), article.content)
-		articles.append(article)
+    for child in content_path.iterdir():
+        filename = child.stem
+        article = get_article(child)
+        article_template = env.get_template("article.html")
+        article.content = article_template.render(article=article.infos(), site=site)
+        write_html(build_path / "article" / (filename + ".html"), article.content)
+        articles.append(article)
 
-	index_content = env.get_template("index.html").render(site=site, articles=articles)
-	write_html(build_path / "index.html", index_content)
+    index_content = env.get_template("index.html").render(site=site, articles=articles)
+    write_html(build_path / "index.html", index_content)
